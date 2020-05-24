8907aba3e99d29de195dc52be81019c49fb275d7
Gary Ascuy <gary.ascuy@gmail.com>
2020-05-24 00:46:12 -0500

    Updating templates to support OS packages

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,19 @@ use serde_json::json;
 mod template;
 
 fn main() {
-    let hbs = template::register_templates();
+    let hbs = &template::register_templates();
 
-    let data = &json!({"name": "foo"});
-    let dockerfile = template::dockerfile(&hbs, data);
-    let dockerignore = template::dockerignore(&hbs, data);
-    
+    let data = &json!({
+        "name": "gary",
+        "maintainer": "Gory Ascuy <gary.ascuy@gmail.com>",
+        "packages": {
+            "build": "",
+            "image": "", // "imagemagick"
+        },
+    });
+    let dockerfile = template::dockerfile(hbs, data);
+    let dockerignore = template::dockerignore(hbs, data);
+
     // File Create
     // let path = Path::new("./Dockerfile");
     // let mut file = File::create(&path).expect("There is not possible update files");
