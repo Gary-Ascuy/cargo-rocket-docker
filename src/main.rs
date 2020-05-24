8ad28ba7c0efb60d701a1d3c01692acbbb643c78
Gary Ascuy <gary.ascuy@gmail.com>
2020-05-24 01:57:30 -0500

    Updating file generation

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,14 @@ use serde_json::json;
 mod template;
 
 fn main() {
-    let hbs = &template::register_templates();
-
     let data = &json!({
-        "name": "gary",
-        "maintainer": "Gory Ascuy <gary.ascuy@gmail.com>",
+        "name": "server",
+        "maintainer": "Gary Ascuy <gary.ascuy@gmail.com>",
         "packages": {
             "build": "",
             "image": "", // "imagemagick"
         },
     });
-    let dockerfile = template::dockerfile(hbs, data);
-    let dockerignore = template::dockerignore(hbs, data);
 
-    // File Create
-    // let path = Path::new("./Dockerfile");
-    // let mut file = File::create(&path).expect("There is not possible update files");
-    // file.write_all(template.as_bytes()).expect("Gary");
-
-    println!("{}", dockerfile);
-    println!("{}", dockerignore);
+    template::save("./example", data);
 }
--- src/template.rs
+++ src/template.rs
@@ -1,23 +1,44 @@
-use handlebars::Handlebars;
-use serde;
+use std::path::{Path, PathBuf};
+use std::io::Write;
+use std::fs::File;
 
-pub fn register_templates() -> handlebars::Handlebars<'static> {
+use handlebars::Handlebars;
+use serde::ser::Serialize;
+
+pub fn register_templates() -> Handlebars<'static> {
     let mut hbs = Handlebars::new();
     hbs.register_template_file("Dockerfile", "./templates/Dockerfile.hbs").expect("Could not read ./template/Dockerfile.hbs file.");
     hbs.register_template_file(".dockerignore", "./templates/.dockerignore.hbs").expect("Could not read ./template/.dockerignore.hbs file.");
     hbs
 }
 
-pub fn dockerfile<T>(hbs: &handlebars::Handlebars<'static>, data: &T) -> String
+pub fn dockerfile<T>(hbs: &Handlebars<'static>, data: &T) -> String
 where
-    T: serde::ser::Serialize
+    T: Serialize
 {
     hbs.render("Dockerfile", data).expect("Could not render docker tempalte")
 }
 
-pub fn dockerignore<T>(hbs: &handlebars::Handlebars<'static>, data: &T) -> String
+pub fn dockerignore<T>(hbs: &Handlebars<'static>, data: &T) -> String
 where
-    T: serde::ser::Serialize
+    T: Serialize
 {
     hbs.render(".dockerignore", data).expect("There is a problem with template")
+}
+
+pub fn save_file(content: &str, path: &PathBuf) {
+    let mut file = File::create(&path).expect("There is not possible update files");
+    file.write_all(content.as_bytes()).expect("Could not save data into file");
+}
+
+pub fn save<T>(project_path: &str, data: &T)
+where
+    T: Serialize
+{
+    let hbs = &register_templates();
+    let path = Path::new(project_path).canonicalize().expect("Could not found project path.");
+    let dir = Path::new(path.as_os_str());
+
+    save_file(&dockerfile(hbs, data), &dir.join("Dockerfile"));
+    save_file(&dockerignore(hbs, data), &dir.join(".dockerignore"));
 }
